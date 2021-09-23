7d483847d8790b79decae93aea65a6018eb1de94
aleksandr.hirs <aleksandr.hirs@gmail.com>
2021-09-23 09:15:38 +0300

    APi monkey Web design - page 2 - container

--- src/main/java/com/skg/apimonkey/controller/ApiMonkeyController.java
+++ src/main/java/com/skg/apimonkey/controller/ApiMonkeyController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class ApiMonkeyController {
 
@@ -15,5 +18,17 @@ public class ApiMonkeyController {
         model.addAttribute("robots", "index");
 
         return "index";
+    }
+
+    @GetMapping("/get-started")
+    public String scholarshipSearch(Model model,
+                                    HttpServletRequest request,
+                                    HttpServletResponse response) {
+
+        model.addAttribute("title", "Get started");
+        model.addAttribute("description", "Get started with testing your API's");
+        model.addAttribute("robots", "noindex");
+
+        return "run_page";
     }
 }
--- src/main/resources/rocket/gulpfile.js
+++ src/main/resources/rocket/gulpfile.js
@@ -109,6 +109,7 @@ gulp.task('serve', gulp.series('scss', 'html', 'index', 'assets', 'vendor', func
         [
             paths.src.scss + '/rocket/**/*.scss',
             paths.src.scss + '/rocket.scss',
+            paths.src.scss + '/run_page.scss',
             paths.src.scss + '/index.scss'
         ], gulp.series('clean:dist', 'copy:dist:css', 'copy:dist:html', 'copy:dist:html:index', 'copy:dist:assets', 'minify:css', 'minify:html', 'minify:html:index'));
     gulp.watch(
@@ -126,7 +127,8 @@ gulp.task('serve', gulp.series('scss', 'html', 'index', 'assets', 'vendor', func
 gulp.task('beautify:css', function () {
     return gulp.src([
         paths.dev.css + '/rocket.css',
-        paths.dist.css + '/index.css'
+        paths.dev.css + '/run_page.css',
+        paths.dev.css + '/index.css'
     ])
         .pipe(cssbeautify())
         .pipe(gulp.dest(paths.dev.css))
@@ -136,6 +138,7 @@ gulp.task('beautify:css', function () {
 gulp.task('minify:css', function () {
     return gulp.src([
         paths.dist.css + '/rocket.css',
+        paths.dist.css + '/run_page.css',
         paths.dist.css + '/index.css'
     ])
         .pipe(cleanCss())
@@ -183,6 +186,7 @@ gulp.task('copy:dist:css', function () {
     return gulp.src([
         paths.src.scss + '/rocket/**/*.scss',
         paths.src.scss + '/rocket.scss',
+        paths.src.scss + '/run_page.scss',
         paths.src.scss + '/index.scss'
     ])
         .pipe(wait(500))
